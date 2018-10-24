7cb7502cbada12fa72ec3b6faae5d6b6171834de
kaiolae <jallafsen@gmail.com>
2018-10-24 13:18:49 +0200

    Modified to allow exploring effects of varying number of mixtures

---
+++ doomrnn/explore_num_mixtures.bash
@@ -0,0 +1,6 @@
+python rnn_train.py --epochs 51 --num_mixtures 1
+python rnn_train.py --epochs 51 --num_mixtures 2
+python rnn_train.py --epochs 51 --num_mixtures 4
+python rnn_train.py --epochs 51 --num_mixtures 8
+python rnn_train.py --epochs 51 --num_mixtures 16
+
